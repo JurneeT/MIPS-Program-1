9f0ff0d523428195492ff38d5e2e4586982bad7d
Jurnee Tipton <jurnee.tipton@bison.howard.edu>
2018-10-19 20:22:39 -0400

    I finished outputting the first line of the required assignment which is mID number

--- mips1.asm
+++ mips1.asm
@@ -8,6 +8,27 @@
  li $v0, 1 
  li $a0, 0
  syscall
+ li $v0, 1 
+ li $a0, 2
+ syscall
+ li $v0, 1 
+ li $a0, 8
+ syscall
+ li $v0, 1 
+ li $a0, 3
+ syscall
+ li $v0, 1 
+ li $a0, 4
+ syscall
+ li $v0, 1 
+ li $a0, 2
+ syscall
+ li $v0, 1 
+ li $a0, 1
+ syscall
+ li $v0, 1 
+ li $a0, 8
+ syscall 
  
 
  
