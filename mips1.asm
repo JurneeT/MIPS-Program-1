48671b225cb983ead3dac28c3cff51a3b5c65206
Jurnee Tipton <jurnee.tipton@bison.howard.edu>
2018-10-21 19:24:41 -0400

    Update mips1.asm
    I outputted the @ symbol (64), then the number zero by subtracting 16 (48), then the number two by adding 2 (50).

--- mips1.asm
+++ mips1.asm
@@ -4,31 +4,13 @@
  main: 
  li $v0, 11
  li $a0, 64 
- syscall 
- li $v0, 1 
- li $a0, 0
  syscall
- li $v0, 1 
- li $a0, 2
- syscall
- li $v0, 1 
- li $a0, 8
- syscall
- li $v0, 1 
- li $a0, 3
- syscall
- li $v0, 1 
- li $a0, 4
- syscall
- li $v0, 1 
- li $a0, 2
- syscall
- li $v0, 1 
- li $a0, 1
- syscall
- li $v0, 1 
- li $a0, 8
- syscall 
  
+li $v0, 11
+sub $a0, $a0, 16 
+syscall
 
- 
+li $v0, 11
+add $a0, $a0, 2 
+syscall
+
