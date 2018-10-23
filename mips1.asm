25bb2aecde9e5d90a4199bc673f2952d8286c81c
Jurnee Tipton <jurnee.tipton@bison.howard.edu>
2018-10-23 18:53:30 -0400

    Update mips1.asm
    outpuuted an 8 and a 3

--- mips1.asm
+++ mips1.asm
@@ -15,65 +15,14 @@ li $v0, 1
 addi $a0, $a0, 2
 syscall
 
-li $v0, 11
-sub $a0, $a0, 46
+li $v0, 1
+addi $a0, $a0, 6
+syscall 
+
+li $v0, 1
+subi $a0, $a0, 5
 syscall
 
-li $v0, 11
-add $a0, $a0, 74
-syscall
-
-li $v0, 11
-add $a0, $a0, 21 
-syscall
-
-li $v0, 11
-add $a0, $a0, 7
-syscall
-
-li $v0, 11
-add $a0, $a0, 4 
-syscall
-
-li $v0, 11
-sub $a0, $a0, 5
-syscall
-
-li $v0, 11
-sub $a0, $a0, 1 
-syscall
-
-li $v0, 11
-sub $a0, $a0, 66 
-syscall
-
-li $v0, 11
-sub $a0, $a0, 12 
-syscall
-
-li $v0, 11
-add $a0, $a0, 42 
-syscall
-
-li $v0, 11
-add $a0, $a0, 43
-syscall
-
-li $v0, 11
-sub $a0, $a0, 3 
-syscall
-
-li $v0, 11
-sub $a0, $a0, 4
-syscall
-
-li $v0, 11
-sub $a0, $a0, 9 
-syscall
-
-li $v0, 11
-add $a0, $a0, 0 
-syscall
 
 
 
