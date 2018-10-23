8c238206e651f21572f82471048edad105a4dbaa
Jurnee Tipton <jurnee.tipton@bison.howard.edu>
2018-10-23 18:52:19 -0400

    Update mips1.asm
    subtracted 10 from $v0 to tell computer to output an integer, then outputted the a 0 and a 2

--- mips1.asm
+++ mips1.asm
@@ -6,36 +6,13 @@
  li $a0, 64 
  syscall
  
-li $v0, 11
-sub $a0, $a0, 16 
+#ID Number
+sub $v0, $v0, 10 # Subtract 10 to tell computer to ouput an integer  
+sub $a0, $a0, 64 #0 
 syscall
 
-li $v0, 11
-add $a0, $a0, 2 
-syscall
-
-li $v0, 11
-add $a0, $a0, 6 
-syscall
-
-li $v0, 11
-sub $a0, $a0, 5 
-syscall
-
-li $v0, 11
-add $a0, $a0, 1 
-syscall
-
-li $v0, 11
-sub $a0, $a0, 2 
-syscall
-
-li $v0, 11
-sub $a0, $a0, 1 
-syscall
-
-li $v0, 11
-add $a0, $a0, 7 
+li $v0, 1
+addi $a0, $a0, 2
 syscall
 
 li $v0, 11
@@ -89,6 +66,16 @@ syscall
 li $v0, 11
 sub $a0, $a0, 4
 syscall
+
+li $v0, 11
+sub $a0, $a0, 9 
+syscall
+
+li $v0, 11
+add $a0, $a0, 0 
+syscall
+
+
 
 
 
