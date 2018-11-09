727b8594b894a28a2721032d414d9fd1b479c356
JurneeT <jurnee.tipton@bison.howard.edu>
2018-11-08 22:59:39 -0500

    Changed every "subi" to "addi" and added negative numbers

--- mips1.asm
+++ mips1.asm
@@ -1,4 +1,4 @@
- .data
+.data
  
  .text 
  main: 
@@ -20,7 +20,7 @@ addi $a0, $a0, 6
 syscall 
 
 li $v0, 1
-subi $a0, $a0, 5
+addi $a0, $a0, -5
 syscall
 
 li $v0, 1
@@ -28,11 +28,11 @@ addi $a0, $a0, 1
 syscall
 
 li $v0, 1
-subi, $a0, $a0, 2
+addi, $a0, $a0, -2
 syscall
 
 li $v0, 1
-subi, $a0, $a0, 1
+addi, $a0, $a0, -1
 syscall
 
 li $v0, 1
@@ -60,19 +60,19 @@ addi $a0, $a0, 4
 syscall
 
 li $v0, 11
-subi $a0, $a0, 5
+addi $a0, $a0, -5
 syscall
 
 li $v0, 11
-subi $a0, $a0, 1
+addi $a0, $a0, -1
 syscall
 
 li $v0, 11
-subi $a0, $a0, 66
+addi $a0, $a0, -66
 syscall
 
 li $v0, 11
-subi $a0, $a0, 12
+addi $a0, $a0, -12
 syscall
 
 li $v0, 11
@@ -84,36 +84,122 @@ addi $a0, $a0, 43
 syscall
 
 li $v0, 11
-subi $a0, $a0, 3
+addi $a0, $a0, -3
 syscall
 
 li $v0, 11
-subi $a0, $a0, 4
+addi $a0, $a0, -4
 syscall
 
 li $v0, 11
-subi $a0, $a0, 9
+addi $a0, $a0, -9
 syscall
 
 li $v0, 11
 addi $a0, $a0, 0
 syscall
 
+#li $v0, 10
+#syscall.data
+ 
+ .text 
+ main: 
+ li $v0, 11
+ li $a0, 64 
+ syscall
+ 
+#ID Number
+sub $v0, $v0, 10 # Subtract 10 to tell computer to ouput an integer  
+sub $a0, $a0, 64 #0 
+syscall
 
+li $v0, 1
+addi $a0, $a0, 2
+syscall
 
+li $v0, 1
+addi $a0, $a0, 6
+syscall 
 
+li $v0, 1
+addi $a0, $a0, -5
+syscall
 
+li $v0, 1
+addi $a0, $a0, 1
+syscall
 
+li $v0, 1
+addi, $a0, $a0, -2
+syscall
 
+li $v0, 1
+addi, $a0, $a0, -1
+syscall
 
+li $v0, 1
+addi $a0, $a0, 7
+syscall
 
+addi $v0, $v0, 10 #back to character
+addi $a0, $a0, 2
+syscall
 
+li $v0, 11
+addi $a0, $a0, 74
+syscall
 
+li $v0, 11
+addi $a0, $a0, 21
+syscall
 
+li $v0, 11
+addi $a0, $a0, 7
+syscall
 
+li $v0, 11
+addi $a0, $a0, 4
+syscall
 
+li $v0, 11
+addi $a0, $a0, -5
+syscall
 
+li $v0, 11
+addi $a0, $a0, -1
+syscall
 
+li $v0, 11
+addi $a0, $a0, -66
+syscall
 
+li $v0, 11
+addi $a0, $a0, -12
+syscall
 
+li $v0, 11
+addi $a0, $a0, 42
+syscall
 
+li $v0, 11
+addi $a0, $a0, 43
+syscall
+
+li $v0, 11
+addi $a0, $a0, -3
+syscall
+
+li $v0, 11
+addi $a0, $a0, -4
+syscall
+
+li $v0, 11
+addi $a0, $a0, -9
+syscall
+
+li $v0, 11
+addi $a0, $a0, 0
+syscall
+
+#li $v0, 10
+#syscall
